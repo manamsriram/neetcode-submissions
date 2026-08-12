7618f9337aeac822042760b5510e1ba7e9238efb
Sri Ram Mannam <sriram.mannam10@gmail.com>
2026-08-12 11:42:11 -0700

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # map each course to it's pre-requisite
+        preMap = { i:[] for i in range(numCourses) }
+        # the number of courses dependant on the pre-reqs
+        indegree = [0 for i in range(numCourses)]
+        ans = []
+
+        # map all courses to their prerequisites
+        # also map, how many courses depend on this pre-req
+        for crs, pre in prerequisites:
+            indegree[pre] += 1
+            preMap[crs].append(pre)
+
+        # get all the courses that have no dependencies
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            node = q.popleft()
+            ans.append(node)
+            for nei in preMap[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        return ans[::-1] if len(ans) == numCourses else []
