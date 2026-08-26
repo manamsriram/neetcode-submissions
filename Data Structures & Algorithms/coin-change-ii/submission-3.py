e49241b56e41d66e3e419b60ac13753e471d176f
Sri Ram Mannam <sriram.mannam10@gmail.com>
2026-08-25 22:06:51 -0700

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        coins.sort()
+        # stores number of ways to get to an amount from 0 to amount using 0 to index coins
+        memo = [[-1] * (amount + 1) for _ in range(len(coins) + 1)]
+
+        # pass index of current coin and current target amount
+        def dfs(i, a):
+            # number of ways to get amount of 0
+            if a == 0:
+                return 1
+            # out of bounds
+            if i >= len(coins):
+                return 0
+            if memo[i][a] != -1:
+                return memo[i][a]
+            res = 0
+            if a >= coins[i]:
+                # take this coin
+                res = dfs(i, a - coins[i])
+                # do not take this coin
+                res += dfs(i + 1, a)
+            memo[i][a] = res
+            return res
+        
+        return dfs(0, amount)
